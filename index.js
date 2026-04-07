e79c4379e2445941230a05363072a14247ce2e51
Dinesh107 <dineshjr107@gmail.com>
2026-04-07 19:24:30 +0530

    ninth commit

--- index.js
+++ index.js
@@ -4,12 +4,238 @@
 
 
 // window.alert("Hai hello students");
-/*
 
-window.alert("I like food")
 
-*/
+// window.alert("I like food")
 
-document.getElementById("myH1").textContent = "Hello Students";
 
-document.getElementById("myP").textContent = "I like food";
+
+// document.getElementById("myH1").textContent = "Hello Students";
+
+// document.getElementById("myP").textContent = "I like food"; 
+
+// let var const 
+
+// let x;
+// let y; 
+
+// x = 20;
+// y = 30;
+
+
+
+
+// Pascal -  Project, Class, Constructors
+// Every word 1st letter must be capital
+// Scientific_Calculator
+
+
+// Camel - Methods, object, variables
+// Except word reaminnning words 1st letter must bi in capital and 1st word 1st letter must be in small
+
+// student_Register_Number = 3543333;
+
+// findLargestNumber() {
+    
+// }
+
+// let x = 20;
+// let y = 21;
+
+// console.log(x, y);
+
+// let age = 25;
+// let price = 10.45;
+// let gpa = 2.1;
+
+
+
+// let firstName = "Aslam";
+
+
+// console.log(typeof age);
+// console.log(typeof firstName);
+
+// console.log(`my age is ${age}`);
+// console.log(`the price is ${price}`);
+// console.log(`You gps is ${gpa}`);
+// console.log(`Your 1sy name is ${firstName}`);
+
+// let online = false;
+// let forSale = true;
+
+// console.log(`Student is ${online}`);
+// console.log(`Is this bike is for sale ${forSale}`);
+
+// let fullName = "student";
+// document.getElementById("myH1").textContent = fullName;
+
+
+let students = 30;
+
+// students = students + 1;
+// students = students - 1;
+// students = students * 1;
+// students = students / 1;
+// students = students % 1;
+
+// agumaneted assigment opertor
+// students = students + 1; students += 1 ;
+// students = students + 1; students -= 1 ;
+// students = students + 1; students /= 1 ;
+// students = students + 1; students %= 1 ;
+
+// students++;
+// students--;
+
+// ++students;
+// --students;
+
+
+// a = ++students;
+// console.log(a);
+// console.log(students);
+
+
+
+// let result = 1 + 2 * 3 + 4 ** 2;
+//            1  +   6   +  16 = 23
+// 12 % 5 + 8 / 2;
+// console.log(result);
+
+
+// How to accept usert input 
+
+// window prompt 
+// let username = window.prompt("Wht is your username");
+// console.log(username);
+
+// let username;
+
+// document.getElementById("mySubmit").onclick = function() {
+//     username = document.getElementById("myText").value;
+//     console.log(username);
+
+//      document.getElementById("myH1").textContent = `hello ${username}`;
+
+    
+// }
+
+// let age = window.prompt("How old are you");
+// age = Number(age);
+
+// let x = "idly";
+// let y = "idly";
+// let z = "idly";
+// let x = "";
+// let y = "";
+// let z = "";
+
+// x = Number(x);
+// y = String(y);
+// z = Boolean(z);
+
+// console.log(x, typeof x);
+// console.log(y, typeof y);
+// console.log(z, typeof z);
+
+
+// const pi = 3.14159;
+
+// let radius;
+// let cicircumference;
+
+
+// radius = window.prompt(`Enter the raduis of a circle`);
+
+// radius = Number(radius);
+
+// cicircumference = 2 * pi * radius;
+
+// console.log(cicircumference);
+
+
+// let x = 9;
+// let y = 2;
+// let z = 10;
+
+
+// z = Math.round(x);
+// z = Math.floor(x);
+// z = Math.ceil(x);
+// z = Math.trunc(x)
+// z = Math.pow(x, y);
+// z = Math.sqrt(x);
+// let max = Math.max(x, y, z);
+// let min = Math.min(x, y, z);
+// console.log(min);
+
+
+// let age = 13;
+
+// if(age >= 18) {
+//     console.log("you are old enough to vote");
+// } else {
+//     console.log("You must be 18 or 18+ to vote");
+// }
+
+// let isStudent = true;
+
+// if(isStudent) {
+//     console.log("You are student");
+// } else {
+//     console.log("You are not student");
+// }
+
+// let age = 18;
+// let hasLisense = true;
+
+// if(age >= 18) {
+//     console.log("you are old eneough to drive");
+//     if(hasLisense) {
+//         console.log("you have your lisence");
+//     } else {
+//         console.log("You do not have your linsence yet");
+//     }
+// } else {
+//     console.log("you must be 18 or 18+ to have a lisence");
+// }
+
+// let mark = 988;
+
+// if (mark <= 20) {
+//     console.log("Your grade is D, you are fail you need to improve");
+// } else if(mark >= 20 && mark < 30) {
+//     console.log("Your grade is C, you are     fail you need to improve");
+// } else if(mark >= 30 && mark < 70) {
+//     console.log("Your grade is B");
+// } else if(mark >= 70 && mark <= 100) {
+//     console.log("You grade is A");
+// } else {
+//     console.log("invaild mark");
+// }
+
+// const myCheckbox = document.getElementById("myCheckbox");
+// const visaCardBtn = document.getElementById("visaBtn");
+// const rupayCardBtn = document.getElementById("rupayBtn");
+// const masterCardBtn = document.getElementById("masterBtn");
+// const mySubmit = document.getElementById("mySubmit");
+
+// const subResult = document.getElementById("subResult");
+// const paymentResult = document.getElementById("paymentResult");
+
+
+// mySubmit.onclick = function() {
+//     if(myCheckbox.checked) {
+//        subResult.textContent = `You are subscribed`;
+//     } else {
+//         subResult.textContent = `You are not subscribed`;
+//     }
+// }
+
+
+
+
+
+
+
